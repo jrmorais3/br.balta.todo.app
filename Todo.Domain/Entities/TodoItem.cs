77c47e6484fee9d35e6ec6239844d45a77e3ef97
Júnior Morais <jrmorais3@gmail.com>
2022-07-26 21:37:48 -0300

    Adding ICommand and TodoItem Entity

---
+++ Todo.Domain/Entities/TodoItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Domain.Entities
+{
+    public class TodoItem : Entity
+    {
+        public TodoItem(Guid id, string title, bool done, DateTime date, string user) : base(id)
+        {
+            Title = title;
+            Done = false;
+            Date = date;
+            User = user;
+        }
+
+        public void MarkAsDone()
+        {
+            Done = true;
+        }
+
+        public void MarkAsUndone()
+        {
+            Done = true;
+        }
+
+        public void UpdateTitle(string title)
+        {
+            Title = title;
+        }
+
+        public string Title { get; private set; }
+        public bool Done { get; private set; }
+        public DateTime Date { get; private set; }
+        public string User { get; private set; }
+    }
+}
