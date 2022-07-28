108d32274ccaa6a7d9847f87efdf7583d1290f61
Júnior Morais <jrmorais3@gmail.com>
2022-07-27 21:14:25 -0300

    Adding ICommandResult and GenericCommandResult

---
+++ Todo.Domain/Commands/CreateTodoCommand.cs
@@ -0,0 +1,29 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    public class CreateTodoCommand : Notifiable, ICommand
+    {
+        public string Title { get; private set; }
+        public DateTime Date { get; private set; }
+        public string User { get; private set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract()
+                .Requires()
+                .HasMinLen(Title, 3, "Title", "Por favor, descreva melhor essa tarefa")
+                .HasMinLen(User, 6, "User", "Usuário inválido")
+                );
+
+        }
+    }
+}
