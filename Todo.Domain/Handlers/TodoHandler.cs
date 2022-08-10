f070e8450b2ff1a29a7288cb255ad0d0ba1b927e
Junior Morais <jrmorais3@gmail.com>
2022-08-09 21:29:36 -0300

    Adding TodoHandler and ITodoRepository

---
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -0,0 +1,23 @@
+﻿using Flunt.Notifications;
+using Todo.Domain.Api.Handlers.Contratcs;
+using Todo.Domain.Api.Repositories;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Handlers
+{
+    public class TodoHandler : Notifiable, IHandler<CreateTodoCommand>
+    {
+        private readonly ITodoRepository _repository;
+
+        public TodoHandler(ITodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ICommandResult Handle(CreateTodoCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
