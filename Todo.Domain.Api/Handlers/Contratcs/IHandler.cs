5cd276dbdf1bd09752713ef010cd509eb77f8375
Junior Morais <jrmorais3@gmail.com>
2022-08-09 21:01:32 -0300

    Adding IHandler interface

---
+++ Todo.Domain.Api/Handlers/Contratcs/IHandler.cs
@@ -0,0 +1,9 @@
+﻿using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Api.Handlers.Contratcs
+{
+    public interface IHandler<T> where T : ICommand
+    {
+        ICommandResult Handle(T command);
+    }
+}
